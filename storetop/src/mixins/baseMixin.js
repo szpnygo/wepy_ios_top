0454d98b8627a8e9f715d0c2be541552cc57e941
suzhenpeng <280097765@qq.com>
2018-02-06 22:13:24 +0800

    兼容iPhoneX

--- storetop/src/mixins/baseMixin.js
+++ storetop/src/mixins/baseMixin.js
@@ -3,9 +3,14 @@ import wepy from 'wepy'
 export default class baseMixin extends wepy.mixin {
 
   data = {
+    countryList: ['中国', '美国', '英国'],
+    isx: /iphone10/i.test(wx.getSystemInfoSync().model)
   };
 
   methods = {
+    changeCountry: function () {
+      console.log("change country");
+    },
   };
 
   onShow() {
