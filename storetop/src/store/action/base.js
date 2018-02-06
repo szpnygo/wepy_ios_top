a797e5025f6d046e08d6ec03db49641c08d891b4
neosu <280097765@qq.com>
2018-02-06 17:09:18 +0800

    添加tabbar

--- storetop/src/store/action/base.js
+++ storetop/src/store/action/base.js
@@ -1,15 +1,53 @@
 /**
  * Created by ztadmin on 2018/1/16.
  */
-import { REQUEST_API } from '../types/base'
+import {REQUEST_API_NEW_APP, REQUEST_API_NEW_GAME, REQUEST_API_TOP_FREE, REQUEST_API_TOP_PAID, REQUEST_API_TOP_SELL} from '../types/base'
 import { createAction } from 'redux-actions'
 import {requestData} from '../../common/util'
 
-//https://rss.itunes.apple.com/api/v1/us/ios-apps/top-paid/all/10/explicit.json
-
-export const requestApiData = createAction(REQUEST_API, ()=>{
+export const requestNewApp = createAction(REQUEST_API_NEW_APP, (country)=>{
   return new Promise((resolve,reject) => {
-    requestData("https://rss.itunes.apple.com/api/v1/cn/ios-apps/top-paid/all/100/explicit.json",function (data) {
+    requestData("https://rss.itunes.apple.com/api/v1/"+country+"/ios-apps/new-apps-we-love/all/100/explicit.json",function (data) {
+      resolve(data.feed.results)
+    },function (error) {
+      reject(error)
+    })
+  })
+});
+
+export const requestNewGame = createAction(REQUEST_API_NEW_GAME, (country)=>{
+  return new Promise((resolve,reject) => {
+    requestData("https://rss.itunes.apple.com/api/v1/"+country+"/ios-apps/new-games-we-love/all/100/explicit.json",function (data) {
+      resolve(data.feed.results)
+    },function (error) {
+      reject(error)
+    })
+  })
+});
+
+export const requestTopFree = createAction(REQUEST_API_TOP_FREE, (country)=>{
+  return new Promise((resolve,reject) => {
+    requestData("https://rss.itunes.apple.com/api/v1/"+country+"/ios-apps/top-free/all/100/explicit.json",function (data) {
+      resolve(data.feed.results)
+    },function (error) {
+      reject(error)
+    })
+  })
+});
+
+export const requestTopSeller = createAction(REQUEST_API_TOP_SELL, (country)=>{
+  return new Promise((resolve,reject) => {
+    requestData("https://rss.itunes.apple.com/api/v1/"+country+"/ios-apps/top-grossing/all/100/explicit.json",function (data) {
+      resolve(data.feed.results)
+    },function (error) {
+      reject(error)
+    })
+  })
+});
+
+export const requestTopPaid = createAction(REQUEST_API_TOP_PAID, (country)=>{
+  return new Promise((resolve,reject) => {
+    requestData("https://rss.itunes.apple.com/api/v1/"+country+"/ios-apps/top-paid/all/100/explicit.json",function (data) {
       resolve(data.feed.results)
     },function (error) {
       reject(error)
--- storetop/src/store/reducers/base.js
+++ storetop/src/store/reducers/base.js
@@ -2,17 +2,45 @@
  * Created by ztadmin on 2018/1/16.
  */
 import {handleActions} from 'redux-actions'
-import {REQUEST_API} from '../types/base'
+import {REQUEST_API_NEW_APP, REQUEST_API_NEW_GAME, REQUEST_API_TOP_FREE, REQUEST_API_TOP_PAID, REQUEST_API_TOP_SELL} from '../types/base'
 
 const initialState ={
-  list:[]
+  newAppList:[],
+  newGameList:[],
+  topFreeList:[],
+  topPaidList:[],
+  topSellList:[]
 }
 
 export default handleActions({
-  [REQUEST_API](state, data){
+  [REQUEST_API_NEW_APP](state, data){
     return{
       ...state,
-      list : data.payload
+      newAppList : data.payload
+    }
+  },
+  [REQUEST_API_NEW_GAME](state, data){
+    return{
+      ...state,
+      newGameList : data.payload
+    }
+  },
+  [REQUEST_API_TOP_FREE](state, data){
+    return{
+      ...state,
+      topFreeList : data.payload
+    }
+  },
+  [REQUEST_API_TOP_PAID](state, data){
+    return{
+      ...state,
+      topPaidList : data.payload
+    }
+  },
+  [REQUEST_API_TOP_SELL](state, data){
+    return{
+      ...state,
+      topSellList : data.payload
     }
   }
 },initialState)
