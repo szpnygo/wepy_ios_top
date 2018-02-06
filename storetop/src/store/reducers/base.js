9e2297fa7b7a55cf98eb4842bde34f82519fb610
suzhenpeng <280097765@qq.com>
2018-02-06 21:14:07 +0800

    增加数据缓存

--- storetop/src/store/reducers/base.js
+++ storetop/src/store/reducers/base.js
@@ -3,41 +3,63 @@
  */
 import {handleActions} from 'redux-actions'
 import {REQUEST_API_NEW_APP, REQUEST_API_NEW_GAME, REQUEST_API_TOP_FREE, REQUEST_API_TOP_PAID, REQUEST_API_TOP_SELL} from '../types/base'
+import wepy from 'wepy'
 
 const initialState ={
   newAppList:[],
   newGameList:[],
   topFreeList:[],
   topPaidList:[],
-  topSellList:[]
-}
+  topSellList:[],
+  country:'cn',
+};
 
 export default handleActions({
   [REQUEST_API_NEW_APP](state, data){
+    wepy.setStorage({
+      key: REQUEST_API_NEW_APP,
+      data: data.payload
+    });
     return{
       ...state,
       newAppList : data.payload
     }
   },
   [REQUEST_API_NEW_GAME](state, data){
+    wepy.setStorage({
+      key: REQUEST_API_NEW_GAME,
+      data: data.payload
+    });
     return{
       ...state,
       newGameList : data.payload
     }
   },
   [REQUEST_API_TOP_FREE](state, data){
+    wepy.setStorage({
+      key: REQUEST_API_TOP_FREE,
+      data: data.payload
+    });
     return{
       ...state,
       topFreeList : data.payload
     }
   },
   [REQUEST_API_TOP_PAID](state, data){
+    wepy.setStorage({
+      key: REQUEST_API_TOP_PAID,
+      data: data.payload
+    });
     return{
       ...state,
       topPaidList : data.payload
     }
   },
   [REQUEST_API_TOP_SELL](state, data){
+    wepy.setStorage({
+      key: REQUEST_API_TOP_SELL,
+      data: data.payload
+    });
     return{
       ...state,
       topSellList : data.payload
